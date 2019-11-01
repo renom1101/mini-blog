3819c8ede29c47133acb759cbd4b73ad38381b84
Renat Zaicev <renat.zaicev@vinted.com>
2019-11-01 14:37:37 +0200

    fix strange html.js script bug

--- src/html.js
+++ src/html.js
@@ -11,14 +11,11 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FM3DVD483C"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-FM3DVD483C');
-        </script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-FM3DVD483C"
+        ></script>
+        <script src="./scripts/gtag-config.js"></script>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
